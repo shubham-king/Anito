8938b1ca5a0af2397647317b94b79496940eb72e
Siddhartha6909 <104570057+Siddhartha6909@users.noreply.github.com>
2024-11-20 19:46:43 +0530

    Api Information

--- tohost.php
+++ tohost.php
@@ -1,4 +1,4 @@
 <?php
 // Base API URL configuration
-define('BASE_API_URL', 'https://aniwatch-api-i02m.onrender.com');
+define('BASE_API_URL', 'Your Api Url'); // Host Your Own Api From https://github.com/ghoshRitesh12/aniwatch-api
 ?>
